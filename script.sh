0fc140046b7deb46569afc7fb87212bdd237762e
Yann D <yaningo@users.noreply.github.com>
2021-05-14 19:37:15 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # GET THE PLAN FILE
 FILE="./.terraform/plan/tfplan"
+echo $FILE
 
 # GET THE DIFF
 plan_diff=$(echo "totototo is tititi")
+echo $plan_diff
 
 echo 'export MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"' >> $BASH_ENV
