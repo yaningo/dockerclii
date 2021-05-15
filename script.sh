e6d898d9c59f15588978d8f91756836e5095fa8c
Yann D <yaningo@users.noreply.github.com>
2021-05-15 02:09:51 +0100

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# GET THE PLAN FILE
-echo 'export FILE="./.terraform/plan/tfplan"' >> $BASH_ENV
-
-# GET THE DIFF
-echo 'export plan_diff=$(echo "totototo is tititi")' >> $BASH_ENV
-
-echo 'export MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"' >> $BASH_ENV
