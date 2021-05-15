bb3b211ca1294b8cdf6cbc30138a13da93463953
Yann D <yaningo@users.noreply.github.com>
2021-05-15 03:39:12 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# # GET THE PLAN FILE
-# echo ' export FILE="./.terraform/plan/tfplan" ' >> $BASH_ENV
-# echo $FILE
+# GET THE PLAN FILE
+echo ' export FILE="./.terraform/plan/tfplan" ' >> $BASH_ENV
+echo $FILE
 
-# # GET THE DIFF
-# echo 'export plan_diff=$(echo "totototo is tititi")' >> $BASH_ENV
-# echo $plan_diff
+# GET THE DIFF
+echo 'export plan_diff=$(echo "totototo is tititi")' >> $BASH_ENV
+echo $plan_diff
 
-# echo 'export MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"' >> $BASH_ENV
-# echo $MESSAGE
-
-
-FILE="./.terraform/plan/tfplan"; plan_diff="totototo is tititi"; echo 'export MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"' >> $BASH_ENV
-source $BASH_ENV
+echo 'export MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"' >> $BASH_ENV
 echo $MESSAGE
+
+
+# FILE="./.terraform/plan/tfplan"; plan_diff="totototo is tititi"; MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"
+# echo $MESSAGE
