3b31abd04859d40b22f1213c96b9e0002b4f0ede
Yann D <yaningo@users.noreply.github.com>
2021-09-30 22:55:28 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # GET THE PLAN FILE
 # echo ' export FILE="./.terraform/plan/tfplan" ' >> $BASH_ENV
-FILE="./.terraform/plan/tfplan"
-echo $FILE
+# FILE="./.terraform/plan/tfplan"
+# echo $FILE
 
 # GET THE DIFF
 # echo 'export plan_diff=$(echo "totototo is tititi")' >> $BASH_ENV
-plan_diff=$(echo "totototo is tititi")
-echo $plan_diff
-echo $FILE
-source $BASH_ENV
+# plan_diff=$(echo "totototo is tititi")
+# echo $plan_diff
+# echo $FILE
+# source $BASH_ENV
 
-echo 'export MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"' >> $BASH_ENV; source $BASH_ENV
-echo $plan_diff
-echo $FILE
-echo $MESSAGE
-source $BASH_ENV
+# echo 'export MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"' >> $BASH_ENV; source $BASH_ENV
+# echo $plan_diff
+# echo $FILE
+# echo $MESSAGE
+# source $BASH_ENV
 # FILE="./.terraform/plan/tfplan"; plan_diff="totototo is tititi"; MESSAGE="Changes to be applied from terraform: \`\`\`$plan_diff\`\`\`"
 # echo $MESSAGE
+
+cat notif.txt|awk '{printf "%s\\n", $0}'|sed 's/\"/\\"/g' > message.txt
+echo 'export MESSAGE="*Our Changes to be applied from terraform:* \`\`\`$(cat message.txt)\`\`\`" ' >> $BASH_ENV
+echo $MESSAGE
