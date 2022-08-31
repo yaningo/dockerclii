f38ad36cda842551c55e68520b8e06ea148c51a3
Yann D <yaningo@users.noreply.github.com>
2022-08-31 01:28:05 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,18 @@
 import os
 import subprocess
 
-cmd = 'pwd'
+cmd = "pip3 install yfinance"
 subprocess.call(cmd, shell=True)
 
-#import yfinance as yf
+import yfinance as yf
+
 #define the ticker symbol
-#tickerSymbol = 'TSLA'
+tickerSymbol = 'TSLA'
+
 #get data on this ticker
-#tickerData = yf.Ticker(tickerSymbol)
+tickerData = yf.Ticker(tickerSymbol)
+
 #get the historical prices for this ticker
-#tickerDf = tickerData.history(period='1d')
-#print(tickerDf)
+tickerDf = tickerData.history(period='1d')
+
+print(tickerDf)
